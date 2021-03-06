03eb30cbc604976025dccadb310f80d23872620e
osman <osmanvd2006@gmail.com>
2021-03-05 20:56:23 -0500

    added an explorer tab

--- app/src/main/java/com/example/worldnoises/MainActivity.java
+++ app/src/main/java/com/example/worldnoises/MainActivity.java
@@ -2,15 +2,36 @@ package com.example.worldnoises;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int num = 0;
+    Button enter_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        enter_button = findViewById(R.id.enter_button);
+
+        enter_button.setOnClickListener(new View.OnClickListener(){ 
+            final Intent i = new Intent(getApplicationContext(), explorer.class);
+
+            @Override
+            public void onClick(View view) {
+               startActivity(i);
+
+            }
+        });
 
     }
+
+
+
+
 }
