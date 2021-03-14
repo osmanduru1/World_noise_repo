fb4fa559c858843c641f891f7dd6328dc39fadac
osman <osmanvd2006@gmail.com>
2021-03-13 19:36:03 -0500

    lesson 2

--- app/src/main/java/com/example/worldnoises/MainActivity.java
+++ app/src/main/java/com/example/worldnoises/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.worldnoises;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences myprefs;
+    String mySharedPrefFileName = "Login_Info";
     int num = 0;
     Button enter_button;
 
@@ -19,15 +23,26 @@ public class MainActivity extends AppCompatActivity {
 
         enter_button = findViewById(R.id.enter_button);
 
-        enter_button.setOnClickListener(new View.OnClickListener(){ 
+        enter_button.setOnClickListener(new View.OnClickListener() {
             final Intent i = new Intent(getApplicationContext(), explorer.class);
 
             @Override
             public void onClick(View view) {
-               startActivity(i);
+                myprefs = getSharedPreferences(mySharedPrefFileName, Context.MODE_PRIVATE);
 
+
+                if(myprefs.getInt("IS_LOGGED_IN",0) ==0) {
+                    enter_button.setText("PLEASE LOG IN");
+                } else {
+                    enter_button.setText("YOU ARE LOGGED IN");
+                }
+                finish();
             }
+
+
         });
+
+
 
     }
 
